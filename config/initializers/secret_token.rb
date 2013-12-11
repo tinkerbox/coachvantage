e969530ccf58536205490c582e0eb85b79c5dc92
Martin David <takestar15@martin-david-pc.(none)>
2013-12-11 18:02:20 +0200

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coachvantage::Application.config.secret_token = 'de0c60adbe7985a26ef1476fdf9961432aad39a5db1c496a88008f6c209584eec925169f89484ce1ab828d9b095069095f6dff37a5588217ef2f31f2b9f331d5'
