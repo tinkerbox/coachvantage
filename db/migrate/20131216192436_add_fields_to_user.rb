be0ac305aa3cdca8fc7475e4686b144829af602d
Martin David <takestar15@gmail.com>
2013-12-24 16:29:10 +0200

    Heroku config

--- db/migrate/20131216192436_add_fields_to_user.rb
+++ db/migrate/20131216192436_add_fields_to_user.rb
@@ -1,9 +1,9 @@
 class AddFieldsToUser < ActiveRecord::Migration
   def change
-  	add_column :users, :birthday, :date
-  	add_column :users, :sex, :integer
-  	add_column :users, :tel, :string
-  	add_column :users, :skype_id, :string
-  	add_column :users, :avatar, :string
+  	# add_column :users, :birthday, :date
+  	# add_column :users, :sex, :integer
+  	# add_column :users, :tel, :string
+  	# add_column :users, :skype_id, :string
+  	# add_column :users, :avatar, :string
   end
 end
